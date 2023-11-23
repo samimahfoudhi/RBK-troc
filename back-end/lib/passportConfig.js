a574815dcb7cbdc60bd914b1ac04a17c2db668cc
HamzaKsontini <hamzaksontinii@gmail.com>
2023-11-23 19:57:11 +0100

    secondpuh

---
+++ back-end/lib/passportConfig.js
@@ -0,0 +1,60 @@
+const passport = require("passport");
+const Strategy = require("passport-local").Strategy;
+
+const passportJWT = require("passport-jwt");
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+
+const User = require("../models/User");
+const authKeys = require("./authKeys");
+
+const filterJson = (obj, unwantedKeys) => {
+  const filteredObj = {};
+  Object.keys(obj).forEach((key) => {
+    if (unwantedKeys.indexOf(key) === -1) {
+      filteredObj[key] = obj[key];
+    }
+  });
+  return filteredObj;
+};
+
+passport.use(
+  new Strategy(
+    {
+      usernameField: "email",
+      passReqToCallback: true,
+    },
+    (req, email, password, done, res) => {
+      // console.log(email, password);
+      User.findOne({ email: email }, (err, user) => {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false, {
+            message: "User does not exist",
+          });
+        }
+
+        user
+          .login(password)
+          .then(() => {
+            // let userSecure = {};
+            // const unwantedKeys = ["password", "v"];
+            // Object.keys(user["_doc"]).forEach((key) => {
+            //   if (unwantedKeys.indexOf(key) === -1) {
+            //     userSecure[key] = user[key];
+            //   }
+            // });
+            user["_doc"] = filterJson(user["_doc"], ["password", "v"]);
+            return done(null, user);
+          })
+          .catch((err) => {
+            return done(err, false, {
+              message: "Password is incorrect.",
+            });
+          });
+      });
+    }
+  )
+);
